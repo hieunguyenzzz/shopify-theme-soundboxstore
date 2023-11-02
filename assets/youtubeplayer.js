ebbae9054c8c5021555e5b0fe49ff150bfbbfe19
Long <ngohoanglongptit@gmail.com>
2023-11-02 17:53:22 +0700

    update

---
+++ assets/youtubeplayer.js
@@ -0,0 +1,10 @@
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubePlayerAPIReady() {
+  window.onYouTubePlayerAPIReadyCalls =
+    window.onYouTubePlayerAPIReadyCalls || [];
+  window.onYouTubePlayerAPIReadyCalls?.forEach((call) => call());
+}
